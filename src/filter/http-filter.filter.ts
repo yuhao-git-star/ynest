167b242454e3e7a8b50ae7fbc7d9e01218a4c39e
yasuoyuhao <yasuoyuhao@gmail.com>
2020-06-29 13:09:29 +0800

    fix(errorfilter): 修正擷取

--- src/filter/http-filter.filter.ts
+++ src/filter/http-filter.filter.ts
@@ -1,19 +1,29 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { ApiResponseService } from '../api-response/api-response.service';
 
 @Catch()
 export class HttpFilterFilter<T> implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
+
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
 
-    response
-      .status(status)
-      .json(
-        ApiResponseService.generateResponse(null, exception.message, false, status, 0)
-      );
+    try {
+      const status = exception.getStatus();
+
+      response
+        .status(status)
+        .json(
+          ApiResponseService.generateResponse(null, exception.message, false, status, 0)
+        );
+    } catch {
+      response
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json(
+          ApiResponseService.generateResponse(null, exception.message, false, HttpStatus.INTERNAL_SERVER_ERROR, 0)
+        );
+    }
   }
 }
